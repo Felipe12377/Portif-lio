8e677bcae82f7ad5589c1e7c0a1193fbd632ad40
Felipe12377 <142449773+Felipe12377@users.noreply.github.com>
2025-05-15 12:23:53 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+let num1;
+let num2;
+let resultado;
+let parOuImpar
+
+num1 = Number(window.prompt("digite aqui o primeiro numero: "))
+
+num2 = Number(window.prompt("digite aqui o segundo numero: "))
+
+resultado = num1 + num2;
+
+parOuImpar = resultado % 2;
+
+
+if(resultado > 10) {
+    alert("o numero " + resultado + " é maior do que 10")
+} else {
+    alert("o numero  " + resultado + " é menor do que 10")
+}
+
+if(parOuImpar == 0 ) {
+    alert("o numero " + resultado + " é par")
+} else {
+    alert("o numero " + resultado + " é ímpar")
+}
+
